3cc26d399542a8b935bf847530f5985c30929819
JonasBjorhei <jonas.kjeks@gmail.com>
2023-01-12 14:12:15 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const buttonWork = document.getElementById("work");
+const buttonMoneyToBank = document.getElementById("moneyToBank");
+const buttonAdd = document.getElementById("add");
+const buttonTakeLoan = document.getElementById("takeLoan");
+const selectComputers = document.getElementById("computers");
+
+let moneyBankElement = document.getElementById("moneyBank");
+let moneyWalletElement = document.getElementById("moneywallet");
+let loanElement = document.getElementById("loan");
+
+
