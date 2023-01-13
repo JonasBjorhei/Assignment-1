1d4bdb3f409213b521018d459e06bd9d4a56e465
JonasBjorhei <jonas.kjeks@gmail.com>
2023-01-13 08:08:29 +0100

    mostly working bank,work

--- app.js
+++ app.js
@@ -5,7 +5,50 @@ const buttonTakeLoan = document.getElementById("takeLoan");
 const selectComputers = document.getElementById("computers");
 
 let moneyBankElement = document.getElementById("moneyBank");
-let moneyWalletElement = document.getElementById("moneywallet");
+let moneyWalletElement = document.getElementById("moneyWallet");
 let loanElement = document.getElementById("loan");
+let moneyInWallet = 0;
+let moneyInBank = 0;
+let loanAmount = 0;
+let bankLoan = false;
+
+function buttonTakeLoanPressed(){
+ loanAmount = window.prompt("How much would you like to loan?");
+ loanAmount = parseInt(loanAmount);
+
+ if(loanAmount<(moneyInBank*2) && bankLoan == false){
+ bankLoan = true;
+ loanElement.innerText = loanAmount;
+ }
+ else {
+    bankLoan = false;
+ }
+
+ console.log(loanAmount);
+};
 
 
+function buttonWorkPressed(){
+    moneyWalletElement.innerText = moneyInWallet +=100; 
+};
+
+function buttonMoneyToBankPressed(){
+    if(bankLoan == true){
+        moneyInBank += moneyInWallet-(moneyInWallet/10);
+        loanAmount -=(moneyInWallet/10)
+        moneyInWallet = 0;
+        moneyBankElement.innerText = moneyInBank;
+        moneyWalletElement.innerText = moneyInWallet;
+        loanElement.innerText = loanAmount;
+    }
+    else{
+        moneyInBank += moneyInWallet;
+        moneyInWallet = 0;
+        moneyBankElement.innerText = moneyInBank;
+        moneyWalletElement.innerText = moneyInWallet;
+    }
+};
+
+buttonTakeLoan.addEventListener("click", buttonTakeLoanPressed);
+buttonWork.addEventListener("click", buttonWorkPressed);
+buttonMoneyToBank.addEventListener("click", buttonMoneyToBankPressed);
