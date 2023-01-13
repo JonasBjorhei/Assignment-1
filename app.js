36dfdcdb184951e8a780dff10b6029bd7d950d07
JonasBjorhei <jonas.kjeks@gmail.com>
2023-01-13 15:17:21 +0100

    Fikset alt på loan, bare siste oppgave igjen

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const buttonMoneyToBank = document.getElementById("moneyToBank");
 const buttonAdd = document.getElementById("add");
 const buttonTakeLoan = document.getElementById("takeLoan");
 const selectComputers = document.getElementById("computers");
+const buttonPayBackLoanPosition = document.getElementById("payBackLoanPosition");
 
 let moneyBankElement = document.getElementById("moneyBank");
 let moneyWalletElement = document.getElementById("moneyWallet");
@@ -10,25 +11,55 @@ let loanElement = document.getElementById("loan");
 let moneyInWallet = 0;
 let moneyInBank = 0;
 let loanAmount = 0;
+let userInput = 0;
 let bankLoan = false;
+let buttonPayBackLoan;
 
 function buttonTakeLoanPressed(){
- loanAmount = window.prompt("How much would you like to loan?");
- loanAmount = parseInt(loanAmount);
+ userInput = window.prompt("How much would you like to loan?");
+ userInput = parseInt(userInput);
 
- if(loanAmount<(moneyInBank*2) && bankLoan == false){
+ if(userInput<(moneyInBank*2) && bankLoan == false && userInput > 0){
     alert("Loan successful");
  bankLoan = true;
+ loanAmount = userInput;
  loanElement.innerText = loanAmount;
 
- //crete new button to repai loan
- }
- else {
+    if(buttonPayBackLoan == null){
+     buttonPayBackLoan = document.createElement("button");
+    buttonPayBackLoan.innerHTML = "Pay back loan";
+    buttonPayBackLoanPosition.appendChild(buttonPayBackLoan);
+    buttonPayBackLoan.addEventListener("click", buttonPayBackLoanPressed);
+    }
+    buttonPayBackLoan.style.visibility = "visible";
+    buttonPayBackLoan.disabled =false;
+    }
+    else {
     alert("Loan unsuccessful")
-    loanAmount = 0;
-    bankLoan = false;
- }
+    buttonPayBackLoan.disabled =true;
+    buttonPayBackLoan.style.visibility = "hidden";
+
+    }
 };
+
+function buttonPayBackLoanPressed(){
+    if(moneyInWallet >= loanAmount){
+    moneyInWallet -=loanAmount;
+    loanAmount =0;
+    moneyWalletElement.innerText = moneyInWallet;
+    loanElement.innerText = loanAmount;
+    bankLoan = false;
+    buttonPayBackLoan.disabled =true;
+    buttonPayBackLoan.style.visibility = "hidden";
+    }
+    else {
+        loanAmount -=moneyInWallet;
+        moneyInWallet = 0;
+        moneyWalletElement.innerText = moneyInWallet;
+        loanElement.innerText = loanAmount;
+    }
+
+}
 
 
 function buttonWorkPressed(){
@@ -44,6 +75,8 @@ function buttonMoneyToBankPressed(){
             moneyInBank -=loanAmount;
             loanAmount = 0;
             bankLoan = false;
+            buttonPayBackLoan.disabled =true;
+            buttonPayBackLoan.style.visibility = "hidden";
         };
         moneyBankElement.innerText = moneyInBank;
         moneyWalletElement.innerText = moneyInWallet;
